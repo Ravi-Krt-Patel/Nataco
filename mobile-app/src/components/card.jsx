65224deac505d592470c4eb9e0553146ae383ad3
Ravi-Krt-Patel <ravikumar030494@gmail.com>
2022-03-04 23:20:08 -0800

    about page

---
+++ mobile-app/src/components/card.jsx
@@ -0,0 +1,14 @@
+
+import "./card.css";
+import {Navbar} from "./navbar"
+export const Card = ()=> {
+	
+
+	return (<>
+	<div className="container">
+		<Navbar/>
+		<div>this is my page</div>
+		<h1 style={{margin:"0px"}} >this is heading</h1>
+	</div>
+	</>)
+}
---
+++ mobile-app/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+import "./navbar.css"
+
+export const Navbar = ()=>{
+
+	return (<>
+	<div className="nav-container">
+		<div className="nav-left">
+			this is left side
+		</div>
+		<div className="nav-right">
+			this is right side
+		</div>
+		<div className="nav-clear"></div>
+	</div>
+	</>)
+}
